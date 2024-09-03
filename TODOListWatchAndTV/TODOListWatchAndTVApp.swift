08c9ce079e15e0957ca5d6a5191048e68e338837
Nikita12G <gulyi08@mail.ru>
2024-09-03 22:58:59 +0700

    Initial Commit

---
+++ TODOListWatchAndTV/TODOListWatchAndTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListWatchAndTVApp.swift
+//  TODOListWatchAndTV
+//
+//  Created by Никита Гуляев on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TODOListWatchAndTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
